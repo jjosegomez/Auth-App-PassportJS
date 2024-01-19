0aa6326de59e66c79468c8611eb9aef938a43307
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2024-01-19 11:02:32 -0500

    parallax done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,75 +1,12 @@
-import { useState } from 'react'
-import axios from 'axios';
 import './App.css'
+import Hero from './components/Hero/Hero'
 
-type User = {
-  email: String,
-  password: String
-}
 
 function App() {
-  const [email, setEmail] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
-
-
-  async function handleRegister(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    console.log("Register", email, password);
-
-    const url: string = "http://localhost:3000/register";
-    const data = {
-      email: email,
-      password: password
-    };
-
-    try {
-      // Since you are using axios.post, it returns a Promise. You should await its response.
-      const response = await axios.post(url, data);
-      console.log(response);
-    } catch (error) {
-      console.log(`error making post request: ${error} with response`);
-    }
-  }
-
-
-  function handleLogin(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    console.log("Login", email, password)
-
-    const url: string = "http://localhost:3000/login"
-    const data: User = {
-      "email": email,
-      "password": password
-    }
-
-    try {
-      const response = axios.post(url, data)
-      console.log(response)
-    } catch (error) {
-      console.log(`error making post request. ${error}`)
-    }
-
-  }
-
-
-
-  return (
-    <>
-      <h1>Register</h1>
-      <form id='registerForm' onSubmit={handleRegister} style={{ display: "flex", justifyContent: "space-around", alignContent: "space-evenly", flexFlow: "column", width: "400px", height: "200px" }} >
-        <input style={{ fontSize: 24 }} type="email" placeholder='enter email' value={email} onChange={(e) => { setEmail(e.target.value) }} required />
-        <input style={{ fontSize: 24 }} type="password" placeholder='enter password' value={password} onChange={(e) => { setPassword(e.target.value) }} required />
-        <button type='submit'>Register</button>
-      </form>
-      <hr />
-      <h1>Login</h1>
-      <form id='loginForm' onSubmit={handleLogin} style={{ display: "flex", justifyContent: "space-around", alignContent: "space-evenly", flexFlow: "column", width: "400px", height: "200px" }} >
-        <input style={{ fontSize: 24 }} type="email" placeholder='enter email' value={email} onChange={(e) => { setEmail(e.target.value) }} required />
-        <input style={{ fontSize: 24 }} type="password" placeholder='enter password' value={password} onChange={(e) => { setPassword(e.target.value) }} required />
-        <button type='submit'>Login</button>
-      </form>
-
-    </>
+  return(
+    <body className='App'>
+      <Hero/>
+    </body>
   )
 }
 
---
+++ client/src/components/Hero/Hero.tsx
@@ -0,0 +1,52 @@
+import cloud2 from "../../assets/parallax/cloud2.png"
+import cloud3 from "../../assets/parallax/cloud3.png"
+import cloud4 from "../../assets/parallax/cloud4.png"
+import mountain_l from "../../assets/parallax/mountain-left.png"
+import mountain_m from "../../assets/parallax/mountain-center.png"
+import mountain_r from "../../assets/parallax/mountain-right.png"
+
+import "./Hero.css"
+
+export default function Hero() {
+  return (
+    <section className='wrapper'>
+      <section className="parallax">
+        <img className="mountain" src={mountain_m}></img>
+        <img className="mountain" src={mountain_l}></img>
+        <img className="cloud" src={cloud2}></img>
+        <h1 style={{fontSize:180, opacity:1, textShadow:"#333 5px", lineHeight:0}}>Firstland</h1>
+        <img className="mountain" src={mountain_r}></img>
+        <img className="cloud" src={cloud3}></img>
+        <img className="cloud" src={cloud4}></img>
+      </section>
+      <section className="text">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eum natus placeat voluptatem blanditiis consequatur. Minus numquam veritatis enim quo, suscipit doloremque perspiciatis, possimus dolor distinctio asperiores libero nisi! Optio.</p>
+    </section>
+    </section>
+  )
+}
